03632a3826a588e785a69f63238583418455703e
Kwakcena <rhkrgudwh@gmail.com>
2020-08-09 12:35:57 +0900

    Install raf and update jest.config.js
    react-slick 라이브러리를 사용하면 jest의 test가 깨지게 된다.
    
    matchMedia not present, legacy browsers require a polyfill
    
    이를 해결하기 위해서는 공식 문서에서 하라는 대로 변경하였다.

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+import 'raf/polyfill';
+
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener() {},
+    removeListener() {},
+  };
+};
