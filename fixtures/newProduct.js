121399964ef640908359e6a8ab57643a0c764763
Kwakcena <rhkrgudwh@gmail.com>
2020-10-18 19:19:50 +0900

    Add fixture newProduct
    새 상품의 추가에 대한 테스트에 쓰일 fixture 데이터를 만들어 사용한다.

---
+++ fixtures/newProduct.js
@@ -0,0 +1,15 @@
+const newProduct = {
+  title: '아이패드 4세대 팝니다.',
+  description: '신형 아이패드 4세대 12.9인치 팝니다. 사용한지 3개월도 안된 제품이에요.',
+  region: '서울 강남',
+  price: '990000',
+  productImages: [
+    'https://via.placeholder.com/600/810b14',
+    'https://via.placeholder.com/600/24f355',
+    'https://via.placeholder.com/600/f66b97',
+  ],
+  createAt: '12341231242',
+  creatorId: 'abc123',
+};
+
+export default newProduct;
