ca311c1f02cccc7f00340c6cf675e5d20012c297
Kwakcena <rhkrgudwh@gmail.com>
2020-10-25 03:02:03 +0900

    Material ui styles
    material ui 컴포넌트에 사용되는 각종 스타일들을 정의하는 styles

---
+++ src/styles/styles.jsx
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+}));
+
+export default useStyles;
