be4c3f151226a16c53428b9574ff5c183887bacd
Kwakcena <rhkrgudwh@gmail.com>
2020-11-15 22:00:48 +0900

    재사용 가능한 FormikField 만들기
    추후에 LoginForm, SignupForm에 재사용하도록 FormikField 컴포넌트를
    만들었다.

--- src/components/presentational/WriteForm.jsx
+++ src/components/presentational/WriteForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { useFormik } from 'formik';
+import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 
 import {
-  TextField, Button, Grid, InputAdornment,
+  Button, Grid, InputAdornment,
 } from '@material-ui/core';
+import FormikField from './FormikField';
+
 import useStyles from '../../styles/styles';
 
 const validationSchema = yup.object({
@@ -26,97 +28,85 @@ const validationSchema = yup.object({
 export default function WriteForm({ onSubmit }) {
   const classes = useStyles();
 
-  const formik = useFormik({
-    initialValues: {
-      title: '',
-      description: '',
-      price: 0,
-      region: '',
-    },
-    validationSchema,
-    onSubmit: () => onSubmit({ newProduct: formik.values }),
-  });
+  function handleSubmit(values) {
+    onSubmit({ newProduct: values });
+  }
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <Grid
-        container
-        spacing={3}
-        className={classes.form}
-      >
-        <Grid item xs={12}>
-          <TextField
-            type="text"
-            label="글 제목"
-            id="write-title"
-            name="title"
-            value={formik.values.title}
-            onChange={formik.handleChange}
-            error={formik.touched.title && Boolean(formik.errors.title)}
-            helperText={formik.touched.title && formik.errors.title}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            type="number"
-            label="상품 가격"
-            id="write-price"
-            name="price"
-            value={formik.values.price}
-            onChange={formik.handleChange}
-            error={formik.touched.price && Boolean(formik.errors.price)}
-            helperText={formik.touched.price && formik.errors.price}
-            fullWidth
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  ￦
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            type="text"
-            label="판매 지역"
-            id="write-region"
-            name="region"
-            value={formik.values.region}
-            onChange={formik.handleChange}
-            error={formik.touched.region && Boolean(formik.errors.region)}
-            helperText={formik.touched.region && formik.errors.region}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            type="text"
-            label="게시글 내용을 작성해주세요"
-            id="write-description"
-            name="description"
-            value={formik.values.description}
-            onChange={formik.handleChange}
-            error={formik.touched.description && Boolean(formik.errors.description)}
-            helperText={formik.touched.description && formik.errors.description}
-            multiline
-            rows={8}
-            variant="outlined"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
-          >
-            글쓰기
-          </Button>
-
-        </Grid>
-      </Grid>
-    </form>
+    <Formik
+      initialValues={{
+        title: '',
+        description: '',
+        price: 0,
+        region: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {
+        ({ touched, errors }) => (
+          <Form>
+            <Grid
+              container
+              spacing={3}
+              className={classes.form}
+            >
+              <Grid item xs={12}>
+                <FormikField
+                  label="글 제목"
+                  id="write-title"
+                  name="title"
+                  error={touched.title && Boolean(errors.title)}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <FormikField
+                  type="number"
+                  label="상품 가격"
+                  id="write-price"
+                  name="price"
+                  error={touched.description && Boolean(errors.description)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        ￦
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <FormikField
+                  label="판매 지역"
+                  id="write-region"
+                  name="region"
+                  error={touched.region && Boolean(errors.region)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormikField
+                  label="게시글 내용을 작성해주세요"
+                  id="write-description"
+                  name="description"
+                  error={touched.description && Boolean(errors.description)}
+                  multiline
+                  rows={8}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                >
+                  글쓰기
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        )
+      }
+    </Formik>
   );
 }
