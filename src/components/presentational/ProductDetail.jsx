f09b4283645cdf2c07d55051606f11fb2f65dfb1
Kwakcena <rhkrgudwh@gmail.com>
2020-11-20 14:17:15 +0900

    이미지가 컨테이너에 꽉 차게 하기
    최소 높이값을 100%로 설정하여 부모 컨테이너의 높이를 따르게 한다.

--- src/components/presentational/ProductDetail.jsx
+++ src/components/presentational/ProductDetail.jsx
@@ -102,6 +102,7 @@ const SliderWrap = styled.div({
 const Image = styled.img({
   position: 'absolute',
   minWidth: '100%',
+  minHeight: '100%',
   left: '50%',
   top: '50%',
   transform: 'translate(-50%, -50%)',
