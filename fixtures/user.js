4afc4daf957a5dd26dbca1a66680dad6d10c1246
Kwakcena <rhkrgudwh@gmail.com>
2020-11-22 16:19:58 +0900

    user test 데이터 추가
    - 로그인 한 유저와 로그아웃 한 유저 가짜 데이터 추가

---
+++ fixtures/user.js
@@ -0,0 +1,16 @@
+const logInUser = {
+  uid: '1234abcd',
+  displayName: '홍 길동',
+  email: 'tester@example.com',
+};
+
+const logOutUser = {
+  uid: '',
+  displayName: '',
+  email: '',
+};
+
+export {
+  logInUser,
+  logOutUser,
+};
