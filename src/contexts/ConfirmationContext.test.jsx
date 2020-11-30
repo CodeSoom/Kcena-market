3ec64e76fface8a053b474d0eabe5a4864b1193a
hannut91 <hannut91@gmail.com>
2020-11-30 23:31:15 +0900

    Add confirm container
    Add ConfirmationContext unit test to test opening modal.

--- src/contexts/ConfirmationContext.test.jsx
+++ src/contexts/ConfirmationContext.test.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { render } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+
+import ConfirmationContext, {
+  ConfirmationProvider,
+} from './ConfirmationContext';
 
 describe('ConfirmationContext', () => {
-  it('Confirmation Context provider', () => {
-    // TODO: Context test
+  const title = '제목';
+  const content = '내용';
+  const open = '열기';
+
+  function TestComponent() {
+    const modal = useContext(ConfirmationContext);
+
+    async function handleClick() {
+      modal.setConfirmForm({ title, content });
+
+      await modal.showConfirmation();
+    }
+
+    return (
+      <button type="button" onClick={handleClick}>{open}</button>
+    );
+  }
+
+  const renderTestComponent = () => render((
+    <ConfirmationProvider>
+      <TestComponent />
+    </ConfirmationProvider>
+  ));
+
+  describe('Open modal', () => {
+    it('shows title and content', () => {
+      const { getByText, queryByText } = renderTestComponent();
+
+      fireEvent.click(getByText(open));
+
+      expect(queryByText(title)).toBeInTheDocument();
+      expect(queryByText(content)).toBeInTheDocument();
+    });
+  });
+
+  describe('Click cancel button', () => {
+    it('close the modal', async () => {
+      const { getByText, queryByText } = renderTestComponent();
+
+      fireEvent.click(getByText(open));
+
+      fireEvent.click(getByText('NO'));
+
+      await waitForElementToBeRemoved(getByText(title));
+
+      expect(queryByText(title)).not.toBeInTheDocument();
+    });
+  });
+
+  describe('Click submit button', () => {
+    it('close the modal', async () => {
+      const { getByText, queryByText } = renderTestComponent();
+
+      fireEvent.click(getByText(open));
+
+      fireEvent.click(getByText('YES'));
+
+      await waitForElementToBeRemoved(getByText(title));
+
+      expect(queryByText(title)).not.toBeInTheDocument();
+    });
   });
 });
