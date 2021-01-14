16771163a4755152e5bc57a55820cf723d83e7d2
Kwakcena <rhkrgudwh@gmail.com>
2021-01-14 21:45:44 +0900

    Remove codeceptjs dependency
    * e2e 테스트는 진행하지 않았으므로 의존성을 제거한다.

--- test/init_test.js
+++
@@ -1,7 +0,0 @@
-Feature('Home');
-
-Scenario('Title을 볼 수 있다.', (I) => {
-  I.amOnPage('/');
-
-  I.see('당근 마켓');
-});
--- test/not_found_test.js
+++
@@ -1,7 +0,0 @@
-Feature('NotFound');
-
-Scenario('존재하지 않는 url 접근 시 NotFound 페이지를 보여준다.', (I) => {
-  I.amOnPage('/any_not_exist_url');
-
-  I.see('404 Not Found');
-});
